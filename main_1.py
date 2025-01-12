2c16fa10696df347d372577d1f390f3018efe1b5
Aleksey <pq1121@mail.ru>
2025-01-12 14:31:29 +0300

    (main_1) add task2

--- main_1.py
+++ main_1.py
@@ -20,3 +20,29 @@ class Animal:
         self.name += other.name
         self.type += other.type
         self.age += other.age
+
+# Task №2
+class Book:
+
+    name: str
+    author: str
+    pages: int
+
+    def __init__(self, name: str, author: str, pages: int):
+        self.name = name
+        self.author = author
+        self.pages = pages
+
+    def open_page(self, page: int):
+        if page <= self.pages:
+            print(f"Открыта станица {page} в книге {self.name}")
+        else:
+            print(f"Стрницы {page} в книге {self.name} нету")
+
+    def info(self):
+        print(f"name: {self.name}, author: {self.author}, pages: {self.pages}")
+
+    def __add__(self, other):
+        self.name += other.name
+        self.author += other.author
+        self.pages += other.pages
